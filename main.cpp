7e5ee3aa06cfb7e75a1cdc87860caa361ac56776
GoldenGarr <maxim20031128@gmail.com>
2022-02-13 10:25:22 +0300

    Started the polynomial

--- main.cpp
+++ main.cpp
@@ -46,44 +46,48 @@ void polyLine_test() {
 }
 
 
-int main() {
-    // point_test();
-    // polyLine_test();
-//    Point p1(0, 0);
-//    Point p2(3, 0);
-//    Point p3(0, 4);
-//    std::vector<Point> vec;
-//    vec.push_back(p1);
-//    vec.push_back(p2);
-//    vec.push_back(p3);
-//    Triangle tr(vec);
-//    Triangle tr2 = tr;
-//    std::cout << tr.space() << std::endl;
-//    std::cout << tr.perimeter();
-
-//    Point p1(1, 1);
-//    Point p2(2, 2);
-//    Point p3(3.5, 2.5);
-//    Point p4(4, 2);
-//    std::vector<Point> vec;
-//    vec.push_back(p1);
-//    vec.push_back(p2);
-//    vec.push_back(p3);
-//    vec.push_back(p4);
-//    Trapezoid trapeze(vec);
-//    std::cout << trapeze.isValid() << std::endl;
-
-    Point p1(1, 1);
-    Point p2(1, 2);
-    Point p3(2, 2);
-    Point p4(2, 2);
-    std::vector<Point> vec;
-
-    vec.push_back(p1);
-    vec.push_back(p2);
-    vec.push_back(p3);
-    vec.push_back(p4);
-
-    RegularPolygon rp(vec);
-    return 0;
-}
+//int main() {
+//    // point_test();
+//    // polyLine_test();
+////    Point p1(0, 0);
+////    Point p2(3, 0);
+////    Point p3(0, 4);
+////    std::vector<Point> vec;
+////    vec.push_back(p1);
+////    vec.push_back(p2);
+////    vec.push_back(p3);
+////    Triangle tr(vec);
+////    Triangle tr2 = tr;
+////    std::cout << tr.space() << std::endl;
+////    std::cout << tr.perimeter();
+//
+////    Point p1(1, 1);
+////    Point p2(2, 2);
+////    Point p3(3.5, 2.5);
+////    Point p4(4, 2);
+////    std::vector<Point> vec;
+////    vec.push_back(p1);
+////    vec.push_back(p2);
+////    vec.push_back(p3);
+////    vec.push_back(p4);
+////    Trapezoid trapeze(vec);
+////    std::cout << trapeze.isValid() << std::endl;
+//
+//
+//
+////    Point p1(1, 1);
+////    Point p2(1, 2);
+////    Point p3(2, 2);
+////    Point p4(2, 2);
+////    std::vector<Point> vec;
+////
+////    vec.push_back(p1);
+////    vec.push_back(p2);
+////    vec.push_back(p3);
+////    vec.push_back(p4);
+////
+////    RegularPolygon rp(vec);
+//
+//
+//    return 0;
+//}
--- polynomial/Polynomial.cpp
+++ polynomial/Polynomial.cpp
@@ -1,39 +1,114 @@
 #include <utility>
+#include <iostream>
 #include <vector>
 
-using namespace std;
+
+class PolynomialElement {
+private:
+    double coefficient;
+    unsigned int power;
+public:
+    PolynomialElement(double coefficient, unsigned int power) {
+        this->coefficient = coefficient;
+        this->power = power;
+    }
+
+    double getCoefficient() const {
+        return coefficient;
+    }
+
+    unsigned int getPower() const {
+        return power;
+    }
+};
 
 class Polynomial {
 private:
-    vector<int> coefficients;
-    size_t polynomial_power;
+    std::vector<PolynomialElement> elements;
+    unsigned int polynomial_power;
 public:
-    Polynomial() : coefficients(), polynomial_power(0) {
+    Polynomial() : elements(), polynomial_power(0) {
     }
 
-    Polynomial(vector<int> coefficients) {
-        this->polynomial_power = coefficients.size();
-        this->coefficients = std::move(coefficients);
-    }
+    ~Polynomial() = default;
 
-    Polynomial(size_t polynomial_power) : coefficients(), polynomial_power(polynomial_power) {
+    Polynomial(std::vector<PolynomialElement> elements) {
+        this->polynomial_power = elements.size();
+        this->elements = std::move(elements);
     }
 
     Polynomial(const Polynomial &p) {
         this->polynomial_power = p.getPolynomialPower();
-        this->coefficients = p.getCoefficients();
+        this->elements = p.getElements();
+    }
+
+    void print() {
+        for (int i = 0; i < elements.size(); ++i) {
+            std::cout << "(" << elements[i].getCoefficient() << "x ** " << elements[i].getPower()
+                      << ")";
+            if (i != elements.size() - 1)
+                std::cout << " + ";
+        }
+        std::cout << std::endl;
     }
 
     Polynomial &operator=(const Polynomial &p) {
         if (this != &p) {
-            this->coefficients = p.getCoefficients();
             this->polynomial_power = p.getPolynomialPower();
+            this->elements = p.getElements();
         }
         return *this;
     }
 
-    const vector<int> &getCoefficients() const {
-        return coefficients;
+    bool operator==(const Polynomial &p) {
+//        for (int i = 0; i < this->polynomial_power; i++) {
+//            if (this->elements[i].getCoefficient() == 0) {
+//                for (int j = 0; j < p.getPolynomialPower(); j++) {
+//                    if (p.getElements()[j].getPower() == this->elements[i].getPower()
+//                        && p.getElements()[j].getCoefficient() != 0) {
+//                        return false;
+//                    }
+//                }
+//            } else {
+//
+//            }
+//        }
+return true;
+    }
+
+    bool operator!=(const Polynomial &p) {
+        return !(*this == p);
+    }
+
+    Polynomial operator+(const Polynomial &p) {
+        int i = 0;
+        int j = 0;
+        Polynomial sum;
+        while (true) {
+            if (elements[i].getPower() == p.elements[j].getPower()) {
+                PolynomialElement element(elements[i].getCoefficient() + p.elements[i].getCoefficient(),
+                                          elements[i].getPower());
+                sum.addPolynomialElement(element);
+                ++i;
+                ++j;
+            }
+
+            if (i > getElements().size()) {
+                break;
+            }
+            if (j > p.getElements().size()) {
+                break;
+            }
+        }
+        return Polynomial();
+    }
+
+    void addPolynomialElement(const PolynomialElement &elem) {
+        elements.push_back(elem);
+    }
+
+    const std::vector<PolynomialElement> &getElements() const {
+        return elements;
     }
 
     size_t getPolynomialPower() const {
@@ -41,4 +116,17 @@ public:
     }
 };
 
+int main() {
+    PolynomialElement p1(1, 3);
+    PolynomialElement p2(2, 2);
+    PolynomialElement p3(-3, 1);
+    PolynomialElement p4(4, 0);
 
+    Polynomial polynomial;
+    polynomial.addPolynomialElement(p1);
+    polynomial.addPolynomialElement(p2);
+    polynomial.addPolynomialElement(p3);
+    polynomial.addPolynomialElement(p4);
+
+    polynomial.print();
+}
